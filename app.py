7209a9cc93624c25152feeb33a2f066438625b9c
artemka8484 <bartenev21@gmail.com>
2026-09-17 02:53:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request, redirect
+
 app = Flask(__name__)
+
+# Временная тестовая база данных
+db = {}
 
 @app.route('/')
 def home():
-    return "AR OK"
+    qr_id = request.args.get('id')
+    
+    if not qr_id:
+        return "Пожалуйста, отсканируйте QR-код."
+        
+    if qr_id not in db:
+        bot_link = "https://t.me/my_magic_ar_bot?start=" + str(qr_id)
+        return redirect(bot_link)
+        
+    return "Здесь будет открываться дополненная реальность (AR)!"
